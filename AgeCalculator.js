3b5f6d62b00cc95195d9725d70de4f54a88861e6
Sri Vastav Polsani <vastavp@gmail.com>
2017-02-21 21:11:56 -0800

    Add files via upload

---
+++ AgeCalculator.js
@@ -0,0 +1,2 @@
+var ageYears = prompt("What is you age?");
+alert("Holy cow! , You've been alive " + ageYears*365 + " days, Die already :D");
---
+++ JavaScriptExercise1.js
@@ -0,0 +1,6 @@
+firstName = prompt("What is your name");
+lastName = prompt("What is your name");
+age = prompt("What is your age");
+
+console.log("It doesn't matter what your name is, OK " + firstName + ' ' + lastName);
+console.log("OK your age is " + age);
